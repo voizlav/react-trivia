71d90247206dbd25ee4434a41479b7924c3c31b3
Vojislav Trujkic <voizlav@gmail.com>
2023-05-24 00:30:36 +0200

    Import URL to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import { CATEGORIES } from "./constants/categories";
 import { API_KEY } from "./constants/api";
+import { URL } from "./constants/url";
 import "./App.css";
 
 function App() {
-  console.log(API_KEY);
+  console.log(URL);
   return <></>;
 }
 
