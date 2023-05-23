3231f2d7f7f7677e0c4de5a32cc60c9bb8ce3a01
Vojislav Trujkic <voizlav@gmail.com>
2023-05-24 00:23:24 +0200

    Import API_KEY to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import { CATEGORIES } from "./constants/categories";
+import { API_KEY } from "./constants/api";
 import "./App.css";
 
 function App() {
+  console.log(API_KEY);
   return <></>;
 }
 
