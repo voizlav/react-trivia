490087e5c216e0cc722a6673d7b3a4242b54b58d
Vojislav Trujkic <voizlav@gmail.com>
2023-05-24 03:04:08 +0200

    Update QuestionList component

--- src/components/QuestionList.jsx
+++ src/components/QuestionList.jsx
@@ -1,5 +1,20 @@
 import { useState } from "react";
+import QButton from "./QButton";
+import Heading from "./Heading";
 
 export default ({ question, handlerBack, handlerAnswer }) => {
-  const [answer, setAnswer] = useState("");
+  const [userAnswer, setUserAnswer] = useState("");
+
+  return (
+    <div className="wrapper">
+      <QButton handler={handlerBack} label={"Back"} />
+      {question ? (
+        <Heading label={question} />
+      ) : (
+        <Heading label={"LOADING DATA..."} />
+      )}
+      <input />
+      <QButton handler={handlerAnswer} label={"Submit"} />
+    </div>
+  );
 };
