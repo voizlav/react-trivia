e46b83478b9d9ccf500b55fbca09e4a571e86d12
Vojislav Trujkic <voizlav@gmail.com>
2023-05-24 02:04:31 +0200

    Add handlePickCategory and useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -10,11 +10,19 @@ import CategoryList from "./components/CategoryList";
 const App = () => {
   const [category, setCategory] = useState("");
 
+  useEffect(() => {
+    category && console.log(category);
+  }, [category]);
+
+  const handlePickCategory = (category) => {
+    setCategory(category);
+  };
+
   return (
     <>
       <div className="wrapper">
         <Heading label={"Web 2.5 Trivia"} />
-        <CategoryList categories={CATEGORIES} />
+        <CategoryList categories={CATEGORIES} handler={handlePickCategory} />
       </div>
     </>
   );
