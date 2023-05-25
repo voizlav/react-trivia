9dd1e80beceb8cbd77ae2ea363b63f5a94499c1c
Vojislav Trujkic <voizlav@gmail.com>
2023-05-25 19:14:53 +0200

    Update className for style error

--- src/components/QuestionList.jsx
+++ src/components/QuestionList.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Button from "./Button";
 import Heading from "./Heading";
 
-export default ({ question, handlerBack, handlerAnswer }) => {
+export default ({ question, handlerBack, handlerAnswer, handlerError }) => {
   const [userAnswer, setUserAnswer] = useState("");
 
   return (
@@ -15,7 +15,7 @@ export default ({ question, handlerBack, handlerAnswer }) => {
       )}
       <input
         onChange={(e) => setUserAnswer(e.target.value)}
-        className="inputDefault"
+        className={`inputDefault ${handlerError && "inputError"}`}
       />
       <Button handler={() => handlerAnswer(userAnswer)} label={"Submit"} />
     </div>
